f9bc7d334527f239cd799c7c621aa4fb29b94b8c
SolidShen <43554090+SolidShen@users.noreply.github.com>
2018-11-16 00:19:41 -0500

    Update model.py
    add batchNormalization module

--- model.py
+++ model.py
@@ -7,23 +7,26 @@ from torch.nn import init
 import numpy as np
 
 def conv3x3(in_channels, out_channels, stride=1, 
-            padding=1, bias=True, groups=1):    
-    return nn.Conv2d(
-        in_channels,
-        out_channels,
-        kernel_size=3,
-        stride=stride,
-        padding=padding,
-        bias=bias,
-        groups=groups)
+            padding=1, bias=True, groups=1):  
+
+
+    return nn.Sequential(nn.Conv2d(
+                                in_channels,
+                                out_channels,
+                                kernel_size=3,
+                                stride=stride,
+                                padding=padding,
+                                bias=bias,
+                                groups=groups),nn.InstanceNorm2d(out_channels))
 
 def upconv2x2(in_channels, out_channels, mode='transpose'):
     if mode == 'transpose':
-        return nn.ConvTranspose2d(
+
+        return nn.Sequential(nn.ConvTranspose2d(
             in_channels,
             out_channels,
             kernel_size=2,
-            stride=2)
+            stride=2),nn.InstanceNorm2d(out_channels)) 
     else:
         # out_channels is always going to be the same
         # as in_channels
@@ -32,12 +35,13 @@ def upconv2x2(in_channels, out_channels, mode='transpose'):
             conv1x1(in_channels, out_channels))
 
 def conv1x1(in_channels, out_channels, groups=1):
-    return nn.Conv2d(
+    return nn.Sequential(nn.Conv2d(
         in_channels,
         out_channels,
         kernel_size=1,
         groups=groups,
-        stride=1)
+        stride=1),nn.InstanceNorm2d(out_channels))
+    
 
 
 class DownConv(nn.Module):
@@ -244,3 +248,4 @@ if __name__ == "__main__":
     out = model(x)
     loss = torch.sum(out)
     loss.backward()
+
