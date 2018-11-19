3577761720dd27df149ad7a384eb10fb5f628d86
SolidShen <43554090+SolidShen@users.noreply.github.com>
2018-11-18 20:58:17 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,7 @@
-class SegmentationDataset(torch.utils.data.Dataset):
+import torchvision.datasets.folder as fold
+import pandas as pd
+
+class SegmentationDatasetBDCLSTM(torch.utils.data.Dataset):
 	def __init__(self, image_dir, label_dir, train, transform_image, transform_label,loader=pd.read_csv):
 		self.image_dir = image_dir
 		self.label_dir = label_dir
@@ -18,16 +21,38 @@ class SegmentationDataset(torch.utils.data.Dataset):
 		if len(self.images) != len(self.labels):
 			raise(RuntimeError("The images and labels are not paired."))
 	def __getitem__(self, index):
+		if index == 0:
+			index = index + 1
+		elif index == len(self.images):
+			index = index -1
 		path_image, index_image = self.images[index]
+		path_image_1, index_image_1 = self.images[index-1]
+		path_image_3, index_image_3 = self.images[index+1]
 		path_label, index_label = self.labels[index]
+		path_label_1, index_label_1 = self.labels[index-1]
+		path_label_3, index_label_3 = self.labels[index+1]
+		
 		image = torch.tensor(self.loader(path_image,header=None).values,dtype = torch.float)
+		image_1 = torch.tensor(self.loader(path_image_1,header=None).values,dtype = torch.float)
+		image_3 = torch.tensor(self.loader(path_image_3,header=None).values,dtype = torch.float)
 		label = torch.tensor(self.loader(path_label,header=None).values)
+		label_1 = torch.tensor(self.loader(path_label_1,header=None).values)
+		label_3 = torch.tensor(self.loader(path_label_3,header=None).values)
 		image_cuda = image.to(device)
+		image_1_cuda = image_1.to(device)
+		image_3_cuda = image_3.to(device)
 		label_cuda = label.to(device)
+		label_1_cuda = label_1.to(device)
+		label_3_cuda = label_3.to(device)
+		
 		if self.transform_image:
 			image_cuda = self.transform_image(image_cuda)
+			image_1_cuda = self.transform_image(image_1_cuda)
+			image_3_cuda = self.transform_image(image_3_cuda)
 		if self.transform_label:
 			label_cuda = self.transform_label(label_cuda)
-		return image_cuda,label_cuda
+			label_1_cuda = self.transform_label(label_1_cuda)
+			label_3_cuda = self.transform_label(label_3_cuda)
+		return image_cuda,image_1_cuda,image_3_cuda,label_cuda,label_1_cuda,label_3_cuda
 	def __len__(self):
 		return len(self.images)
